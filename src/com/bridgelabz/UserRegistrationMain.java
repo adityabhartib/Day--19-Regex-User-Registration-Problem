b8cf631b9c94003681502676b374eefefc5f4221
adityabhartib <adityabharti.ab@gmail.com>
2022-11-05 19:07:01 +0530

    Added : UC1- Validate first Name

--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -2,6 +2,8 @@ package com.bridgelabz;
 
 public class UserRegistrationMain {
     public static void main(String[] args) {
+        Validation validation=new Validation();
+        validation.checkFirstName();
 
     }
 }
