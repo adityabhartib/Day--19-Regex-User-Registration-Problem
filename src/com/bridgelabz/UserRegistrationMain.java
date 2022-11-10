e6869debaad1d3d38bd1e0e66a395da4a14f3930
adityabhartib <adityabharti.ab@gmail.com>
2022-11-10 19:33:02 +0530

    Added :UC9 Email validation clear all samples

--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -3,14 +3,23 @@ package com.bridgelabz;
 public class UserRegistrationMain {
     public static void main(String[] args) {
         Validation validation=new Validation();
+        System.out.println("Enter First Name : ");
         validation.checkFirstName();
+        System.out.println("Enter Last Name : ");
         validation.checkLastName();
+        System.out.println("Enter Email : ");
         validation.checkEmail();
+        System.out.println("Enter PhoneNumber : ");
         validation.checkPhoneNumber();
+        System.out.println("Enter Password as per Rule 1: ");
         validation.checkPasswordRule1();
+        System.out.println("Enter Password as per Rule 2: ");
         validation.checkPasswordRule2();
+        System.out.println("Enter Password as per Rule 3: ");
         validation.checkPasswordRule3();
+        System.out.println("Enter Password as per Rule 4: ");
         validation.checkPasswordRule4();
+        System.out.println("Enter Email : ");
         validation.checkEmail2();
     }
 }
--- src/com/bridgelabz/Validation.java
+++ src/com/bridgelabz/Validation.java
@@ -7,7 +7,6 @@ public class Validation {
     Scanner sc = new Scanner(System.in);
 
     public void checkFirstName() {
-        System.out.println("Enter First Name : ");
         String firstName = sc.nextLine();
         if (Pattern.matches("^[A-Z]{1}[a-z]{3,}", firstName)) {
             System.out.println("First Name is Valid---!!");
@@ -19,7 +18,6 @@ public class Validation {
 
     public void checkLastName() {
         System.out.println("-----------------");
-        System.out.println("Enter Last Name : ");
         String lastName = sc.nextLine();
         if (Pattern.matches("^[A-Z]{1}[a-z]{3,}", lastName)) {
             System.out.println("LastName is Valid---!!");
@@ -31,7 +29,6 @@ public class Validation {
 
     public void checkEmail() {
         System.out.println("-----------------");
-        System.out.println("Enter Email : ");
         String email = sc.nextLine();
         if (Pattern.matches("^[a-zA-Z]{3,}[.]?[A-za-z0-9]{3,}[.]?[a-zA-Z]+[@]{1}[a-z]+[.]?[a-z]*", email)) {
             System.out.println("Email is Valid---!!");
@@ -43,7 +40,6 @@ public class Validation {
 
     public void checkPhoneNumber() {
         System.out.println("-----------------");
-        System.out.println("Enter PhoneNumber : ");
         String phoneNumber = sc.nextLine();
         if (Pattern.matches("^[0-9]+\s[0-9]{10}", phoneNumber)) {
             System.out.println("PhoneNumber is Valid---!!");
@@ -55,7 +51,6 @@ public class Validation {
 
     public void checkPasswordRule1() {
         System.out.println("-----------------");
-        System.out.println("Enter Password : ");
         String password = sc.nextLine();
         if (Pattern.matches("^[a-zA-z]{8,}", password)) {
             System.out.println("Password is Valid---!!");
@@ -67,7 +62,6 @@ public class Validation {
 
     public void checkPasswordRule2() {
         System.out.println("-----------------");
-        System.out.println("Enter Password : ");
         String password = sc.nextLine();
         if (Pattern.matches("^[A-z]+[A-Za-z]{8,}", password)) {
             System.out.println("Password is Valid---!!");
@@ -79,7 +73,6 @@ public class Validation {
 
     public void checkPasswordRule3() {
         System.out.println("-----------------");
-        System.out.println("Enter Password : ");
         String password = sc.nextLine();
         if (Pattern.matches("^[A-Z]+[A-Za-z0-9]{8,}", password)) {
             System.out.println("Password is Valid---!!");
@@ -91,7 +84,6 @@ public class Validation {
 
     public void checkPasswordRule4() {
         System.out.println("-----------------");
-        System.out.println("Enter Password : ");
         String password = sc.nextLine();
         if (Pattern.matches("^[A-Z]+[A-Za-z0-9]{7,}[!@#$%^&*()=]+", password)) {
             System.out.println("Password is Valid---!!");
@@ -103,7 +95,6 @@ public class Validation {
 
     public void checkEmail2() {
         System.out.println("-----------------");
-        System.out.println("Enter Email : ");
         String email = sc.nextLine();
         if (Pattern.matches("^[a-zA-Z]{3,}[+-.]?[A-za-z0-9]*[@]{1}[a-z]+[.]?[a-z]*[.]?[a-z]", email)) {
             System.out.println("Email is Valid---!!");
